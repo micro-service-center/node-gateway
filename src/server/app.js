cf8f4f5c6db16f1747312de82e4fa15486ce0aa3
Frank Yao <frank@neon.local>
2016-11-16 15:46:27 +0800

    restructure server libraries

--- src/server/app.js
+++ src/server/app.js
@@ -1,18 +1,18 @@
 const http = require('http')
 const httpProxy = require('http-proxy')
-const RequestValidator = require('./lib/RequestValidator')
+const RequestHandler = require('./lib/RequestHandler')
 
 // Load Configuration File
 const GATEWAY_CONFIG = require('./gateway.json')
 
 let proxy = httpProxy.createProxyServer()
-let requestValidator = new RequestValidator({ config: GATEWAY_CONFIG })
+let requestHandler = new RequestHandler({ conf: GATEWAY_CONFIG })
 
 // Creates the reverse proxy server
 http.createServer((req, res) => {
 	// Request Handler Parses Request
-	requestValidator.parseRequest(req)
-  if (requestValidator.judgeRequest().rejected) {
+	requestHandler.parseRequest(req)
+  if (requestHandler.judgeRequest().rejected) {
     res.writeHead(401, { 'Content-Type': 'application/json' });
     res.write('request rejected' + req.url + '\n' + JSON.stringify(req.headers, true, 2));
     res.end();
--- src/server/lib/policy/Base.js
+++ src/server/lib/policy/Base.js
@@ -9,10 +9,20 @@ class BasePolicy {
 
 	/**
 	 * Judges whether a request or user object
-	 * @param {object}
+	 * @param {user}
+	 * @return {Boolean}
 	 */
 	static allowed(object) {
 		// Throws unimplemented error
+		throw new Error('Unimplemented')
+	}
+
+	/**
+	 * Judges what is included in the request object
+	 * @param  {request}
+	 * @return {Boolean}
+	 */
+	static has(object) {
 		throw new Error('Unimplemented')
 	}
 }
