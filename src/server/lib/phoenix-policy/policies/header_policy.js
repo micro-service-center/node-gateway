99993d35f47af5879e01fe24517c7f8f0eede8f7
Frank Yao <frank@neon.local>
2016-11-25 15:19:24 +0800

    fix

---
+++ src/server/lib/phoenix-policy/policies/header_policy.js
@@ -0,0 +1,17 @@
+const BasePolicy = require('../base')
+
+/** Class for User Policy */
+class HeaderPolicy extends BasePolicy {
+
+	/**
+	 * Judges whether a request or user object
+	 * @param {user}
+	 * @return {Boolean}
+	 */
+	static _approve(requestValidator) {
+		return true
+	}
+
+}
+
+module.exports = HeaderPolicy
