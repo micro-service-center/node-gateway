49c50a4110bafdfe225ea58e7cd54120aa31e30a
Frank <knarfytrebil@gmail.com>
2016-11-29 22:08:22 +0800

    remove utils folder

--- src/utils/target.js
+++
@@ -1,8 +0,0 @@
-const http = require('http')
-
-// Create the target Server
-http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' })
-  res.write('request successfully proxied to: ' + req.url + '\n' + JSON.stringify(req.headers, true, 2))
-  res.end()
-}).listen(9008)
