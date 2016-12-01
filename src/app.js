c1ceec9fc32cc7039fb109961829bc2d2d6f9dde
Frank Yao <frank@neon.local>
2016-12-01 11:38:56 +0800

    modify app for staging

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const App = require('./server/server')
 
 // Load Configuration File
 const CONF_ROOT = "../conf/gateway"
-const CONF_REALM = "dev"
+const CONF_REALM = "staging"
 const GATEWAY_CONFIG = require(`${CONF_ROOT}/${CONF_REALM}/gateway.json`)
 const ERROR_CONFIG = require(`../policies/error.json`)
 
