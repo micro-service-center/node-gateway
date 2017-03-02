e821df9e0a12ee8a8499274437156706804dec83
SensitiveMix <m18205253820@163.com>
2017-03-02 11:12:26 +0800

    add feature:ignore options

--- src/server/server.js
+++ src/server/server.js
@@ -9,45 +9,49 @@ const redis = require('redis')
 const PolicyFactory = require('cyanide-policy').PolicyFactory
 
 class App {
-  constructor(opt) {
-    this.gateway_conf = opt.gateway_conf
-    this.error_conf = opt.error_conf
-    this.httpServer = null
-    this.redis = redis.createClient({url: opt.gateway_conf.REDIS_URL})
-    this.redis.on("error", (err)=>console.log(err));
+    constructor(opt) {
+        this.gateway_conf = opt.gateway_conf
+        this.error_conf = opt.error_conf
+        this.httpServer = null
+        this.redis = redis.createClient({url: opt.gateway_conf.REDIS_URL})
+        this.redis.on("error", (err) => console.log(err));
 
-  } 
-
-  start () {
-    let policyFactory = new PolicyFactory({
-      policy_path: this.gateway_conf.POLICY_PATH,
-      error_conf: this.error_conf  
-    })
-    let validator_conf = {
-      conf: this.gateway_conf,
-      policyFactory: policyFactory,
-      redis: this.redis
     }
-    // Request Validator
-    let requestValidator = new RequestValidator(validator_conf)
-    // User Validator
-    let userValidator = new UserValidator(validator_conf)
 
-    // Request Handler
-    let requestHandler = new RequestHandler({ 
-      requestValidator: requestValidator,
-      userValidator: userValidator
-    })
+    start() {
+        let policyFactory = new PolicyFactory({
+            policy_path: this.gateway_conf.POLICY_PATH,
+            error_conf: this.error_conf
+        })
+        let validator_conf = {
+            conf: this.gateway_conf,
+            policyFactory: policyFactory,
+            redis: this.redis
+        }
+        // Request Validator
+        let requestValidator = new RequestValidator(validator_conf)
+        // User Validator
+        let userValidator = new UserValidator(validator_conf)
 
-    this.httpServer = http.createServer((req, res) => {
-      requestHandler.handleRequest(req, res)
-    }).listen(this.gateway_conf.PORT)
+        // Request Handler
+        let requestHandler = new RequestHandler({
+            requestValidator: requestValidator,
+            userValidator: userValidator
+        })
 
-  }
+        this.httpServer = http.createServer((req, res) => {
+            if (req.method.toLowerCase() == "options") {
+                res.writeHead(204, {'Content-Type': 'application/json'})
+                res.end()
+            }
+            requestHandler.handleRequest(req, res)
+        }).listen(this.gateway_conf.PORT)
 
-  close() {
-    this.httpServer.close()
-  }
+    }
+
+    close() {
+        this.httpServer.close()
+    }
 }
 
 module.exports = App
